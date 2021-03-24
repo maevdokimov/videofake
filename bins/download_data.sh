f5f579939e153889b8868e969feb70230e99b149
maxim <maximevdfox@gmail.com>
2021-03-24 14:16:10 +0300

    Data script

---
+++ bins/download_data.sh
@@ -0,0 +1,9 @@
+# requires gdown (pip install gdown)
+
+gdown https://drive.google.com/uc?id=1gDnwXq3jbAujyxssvszYOrsvQH1MKZg9
+tar -xvf archive.zip
+rm archive.zip
+
+gdown https://drive.google.com/uc?id=0B7EVK8r0v71pZjFTYXZWM3FlRnM
+tar -xvf img_align_celeba.zip
+rm img_align_celeba.zip
