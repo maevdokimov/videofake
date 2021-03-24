dd5912f223cf7f7ac3da9aa37c9ce56e16b2356e
maxim <maximevdfox@gmail.com>
2021-03-24 14:49:54 +0300

    Data path fix

--- bins/download_data.sh
+++ bins/download_data.sh
@@ -1,9 +1,9 @@
-# requires gdown (pip install gdown)
+# requires gdown (pip install gdown) and unzip
 
 gdown https://drive.google.com/uc?id=1gDnwXq3jbAujyxssvszYOrsvQH1MKZg9
-tar -xvf archive.zip
+unzip archive.zip
 rm archive.zip
 
-gdown https://drive.google.com/uc?id=0B7EVK8r0v71pZjFTYXZWM3FlRnM
-tar -xvf img_align_celeba.zip
+gdown https://drive.google.com/uc?id=1t5Nx1ClaG20hPs1qHkwY_85WL-jqvK61
+unzip img_align_celeba.zip
 rm img_align_celeba.zip
